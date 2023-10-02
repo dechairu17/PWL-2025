6dbe14f40e324bf8767ea62da705c5971694b93a
decha17 <dimasadibayus956@gmail.com>
2023-10-02 14:38:42 +0700

    week 6

--- smt1/dasproprak/week6/jobsheet/Pemilihan2Percobaan108.java
+++ smt1/dasproprak/week6/jobsheet/Pemilihan2Percobaan108.java
@@ -12,9 +12,11 @@ public class Pemilihan2Percobaan108 {
         tahun = Input08.nextInt();
 
         if ((tahun % 4) == 0 ) {
-            if ((tahun % 100) != 0)
-                System.out.println("Tahun Kabisat ");
-                else { 
+            if ((tahun % 400 ) == 0){
+                System.out.println("Tahun kabisat");
+            }else if ((tahun % 100) != 0) {
+                System.out.println("Bukan Tahun Kabisat ");
+            }else { 
                     System.out.println("Bukan tahun kabisat");
                 }
         } else {
