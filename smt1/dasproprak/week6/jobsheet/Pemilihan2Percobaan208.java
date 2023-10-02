6660179580d7d09633ea5d83df26122ad3b873b8
decha17 <dimasadibayus956@gmail.com>
2023-10-02 14:29:44 +0700

    week 6

--- smt1/dasproprak/week6/jobsheet/Pemilihan2Percobaan208.java
+++ smt1/dasproprak/week6/jobsheet/Pemilihan2Percobaan208.java
@@ -16,10 +16,13 @@ public class Pemilihan2Percobaan208 {
         totalSudut = sudut1 + sudut2 + sudut3;
 
         if (totalSudut == 180) {
-            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-                System.out.println("Segitiga tersebut adalah Segitiga siku siku");
-                else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)) {
-                    System.out.println("Segitiga tersebut adalah Segitiga sama sisi");
+            if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60))
+                System.out.println("Segitiga tersebut adalah Segitiga sama sisi");
+                else if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+                    if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3))
+                    System.out.println("Segitiga tersebut adalah Segitiga siku siku sama kaki");   
+                    else  
+                    System.out.println("Segitiga tersebut adalah Segitiga siku siku");
                 }
                     else if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3)) {
                         System.out.println("Segitiga tersebut adalah segitiga sama kaki");
