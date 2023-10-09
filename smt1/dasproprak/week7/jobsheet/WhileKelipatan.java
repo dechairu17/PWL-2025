670ce62b65093ab905cd11e6e0c346e10f9beb12
decha17 <dimasadibayus956@gmail.com>
2023-10-09 11:38:42 +0700

    week 7

--- smt1/dasproprak/week7/jobsheet/WhileKelipatan.java
+++ smt1/dasproprak/week7/jobsheet/WhileKelipatan.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 
 public class WhileKelipatan {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
+        int i=1 ;
+        int kelipatan, jumlah = 0, counter = 0;
+        System.out.print("Masukan bilangan kelipatan: ");
+        kelipatan = sc.nextInt(); 
+        while (i <= 50) {
+            i++;
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+        float avg = jumlah / counter; 
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata rata dari bilangan kelipatan %d adalah %.2f\n " , kelipatan, avg);
     }
 }
