f9b2f8293fc5b691f64adf91377460c786e159fd
decha17 <143620209+decha17@users.noreply.github.com>
2023-10-24 01:11:42 +0700

    week 9

--- smt1/dasproprak/week9/jobsheet/ArrayNilaiRata08.java
+++ smt1/dasproprak/week9/jobsheet/ArrayNilaiRata08.java
@@ -8,13 +8,20 @@ public class ArrayNilaiRata08 {
         int [] nilaiMhs  = new int [10];
         double total =0;
         double rata2;
-
+        
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs [i] = sc.nextInt();
         }
         for(int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
+        }
+        for(int i = 0 ; i < nilaiMhs.length; i++){
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i+1) + " lulus!");
+            }  else {
+                System.out.println("Mahasiswa ke-" + (i+1) + " Tidaklulus!");
+            }
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = " + rata2);
