3386bc778d37b9b40300f60d1b9a0ae445070062
decha17 <143620209+decha17@users.noreply.github.com>
2023-11-02 19:00:11 +0700

    update array no 7 percobaan 1 (week 10)

--- smt1/dasproprak/week10/jobsheet/Bioskop08.java
+++ smt1/dasproprak/week10/jobsheet/Bioskop08.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Bioskop08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String [][] penonton = new String [4][2];
+        String [][] penonton = new String [4][50000000];
         penonton [0][0] = "Amin";
         penonton [0][1] = "Bena";
-        penonton [1][0] = "Candra";
+        penonton [5000][0] = "Candra";
         penonton [1][1] = "Dela";
         penonton [2][0] = "Eka";
         penonton [2][1] = "Farhan";
@@ -16,8 +16,8 @@ public class Bioskop08 {
         penonton [3][1] = "Hana";
 
         System.out.println(penonton.length);
-         for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" +(i+1) + ": " + penonton[i].length );
-         }
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang Baris : " + barisPenonton.length);
+        }
     }
 }
