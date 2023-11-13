e799d6691917ad0a76bf6ed1260fcbab127cddd3
decha17 <143620209+decha17@users.noreply.github.com>
2023-11-13 22:40:19 +0700

    Update Github

--- smt1/dasproprak/week11/jobsheet/NesteedLoop_2341720169.java
+++ smt1/dasproprak/week11/jobsheet/NesteedLoop_2341720169.java
@@ -6,21 +6,33 @@ public class NesteedLoop_2341720169 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double [][] temps = new double [5][7];
-
+        double [] rata2 = new double[5];
+        
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke- " + (i+1));
             for (int j = 0; j < temps[0].length; j++) {
-                System.out.println("Hari ke- " + (j+1) + ": ");
+                System.out.print("Hari ke- " + (j+1) + ": ");
                 temps[i][j] = sc.nextDouble();
+                rata2[i] += temps[i][j]; 
             }
             System.out.println();
         }
-        for (int i = 0; i < temps.length; i++) { 
-            System.out.print("Kota ke-" + (i + 1) + ": "); 
-            for (int j = 0; j < temps[0].length; j++) { 
-                System.out.print(temps[i][j] + " "); 
-            } 
-            System.out.println(); 
-        } 
+        for (int i = 0; i < rata2.length; i++) {
+            rata2[i]/= 7;
+        }
+
+        int k = 1;
+        for (double[] a : temps) {
+            System.out.print("Kota ke-" + k + ": "); 
+            for (double b : a) {
+                System.out.print(b + " ");
+            }
+            System.out.println();
+            k++;
+        }
+        System.out.println();
+        for (int i = 0; i < rata2.length; i++) {
+            System.out.println("Rata Rata dari kota ke-" + (i+1) + ": " +rata2[i]);
+        }
     }
 }
