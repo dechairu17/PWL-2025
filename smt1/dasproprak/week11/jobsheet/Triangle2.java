c894bca15977f99db9af01fd4c628bb3dcfcb9c3
decha17 <143620209+decha17@users.noreply.github.com>
2023-11-14 00:10:29 +0700

    Update tugas

---
+++ smt1/dasproprak/week11/jobsheet/Triangle2.java
@@ -0,0 +1,22 @@
+package week11.jobsheet;
+
+import java.util.Scanner;
+
+public class Triangle2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Masukkan nilai N = "); 
+        int N = sc.nextInt(); 
+        if (N > 3) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k= 1; k <= i; k++) {
+                System.out.print(k);
+        }
+        System.out.println();
+        }
+    }
+ }
+}
---
+++ smt1/dasproprak/week11/jobsheet/kotak.java
@@ -0,0 +1,21 @@
+package week11.jobsheet;
+
+import java.util.Scanner;
+
+public class kotak {
+    public static void main(String[] args) {
+        System.out.print("Masukan Nilai : ");
+        Scanner sc = new Scanner(System.in);
+        int nilai = sc.nextInt();
+        for (int i = 0; i < nilai; i++) {
+            for (int j = 0; j < nilai; j++) {
+                if (i == 0 || i == nilai-1 || j == 0 || j == nilai-1) {
+                    System.out.print(nilai);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
