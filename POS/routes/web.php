633949746c40a7d5287a1d9334243e6a2c932401
dechairu17 <dechairu@gmail.com>
2025-03-07 13:28:30 +0700

    QUERY BUILDER

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 
-use App\Http\Controllers\PosController;
+// use App\Http\Controllers\PosController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController
 // use App\Http\Controllers\HomeController;
 // use App\Http\Controllers\ProductController;
 // use App\Http\Controllers\UserController;
@@ -18,7 +20,11 @@ use App\Http\Controllers\LevelController;
 
 // Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 // Route::get('/sales', [SalesController::class, 'index']);
+Route::get('/', function () {return view('welcome');});
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index'])
+
+
 
 
 
