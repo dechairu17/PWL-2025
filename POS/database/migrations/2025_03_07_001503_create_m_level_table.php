9e1c9ca81d451c14c433ea42db15a38fa04086e8
dechairu17 <dechairu@gmail.com>
2025-03-07 09:32:34 +0700

    Update migrasi

--- POS/database/migrations/2025_03_07_001503_create_m_level_table.php
+++ POS/database/migrations/2025_03_07_001503_create_m_level_table.php
@@ -11,11 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Buat tabel dulu tanpa foreign key
         Schema::create('m_level', function (Blueprint $table) {
-            $table->id();
-            $table->string('level_kode',10)->unique();
-            $table->string('level_nama',100);
+            $table->id(); // Primary Key
+            $table->string('level_nama', 100);
             $table->timestamps();
+        });
+
+        // Baru tambahkan kolom foreign key setelah tabel selesai dibuat
+        Schema::table('m_level', function (Blueprint $table) {
+            $table->unsignedBigInteger('level_id')->nullable()->after('id');
+            $table->foreign('level_id')->references('id')->on('m_level')->onDelete('cascade');
         });
     }
 
