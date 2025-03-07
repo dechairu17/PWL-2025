8be59172cab8b929c97c606eb5bafc28612dffb4
dechairu17 <dechairu@gmail.com>
2025-03-07 13:43:36 +0700

    Jobsheet 6

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4,
+        //     'email' => 'customer1@example.com' // Tambahkan email
+        // ];
+        // UserModel::insert($data);
+        
+
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
         // update data user dengan Eloquent Model
         $data = [
             'nama' => 'Pelanggan Pertama',
