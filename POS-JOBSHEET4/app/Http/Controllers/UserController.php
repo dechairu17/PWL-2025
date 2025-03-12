f92ec6434d9599fab40607df560f55b4fa87e231
dechairu17 <dechairu@gmail.com>
2025-03-13 01:04:16 +0700

    RETRIEVING SINGLE MODELS

--- POS-JOBSHEET4/app/Http/Controllers/UserController.php
+++ POS-JOBSHEET4/app/Http/Controllers/UserController.php
@@ -9,18 +9,30 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $data = [
-            'level_id' => 2,
-            // 'username' => 'manager_dua',
-            // 'nama' => 'Manager 2',
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     // 'username' => 'manager_dua',
+        //     // 'nama' => 'Manager 2',
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
 
-        UserModel::insert($data);
+        // UserModel::insert($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::firstwhere('level_id', 1);
+
+        // $user = UserModel::findOr(1, ['username' , 'nama'], function (){
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username' , 'nama'], function (){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
