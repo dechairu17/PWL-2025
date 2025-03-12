47a2a7661253ae5d6170e8c0345fb5b6da8b641a
dechairu17 <dechairu@gmail.com>
2025-03-13 01:07:46 +0700

    Not Found Exceptions

--- POS-JOBSHEET4/app/Http/Controllers/UserController.php
+++ POS-JOBSHEET4/app/Http/Controllers/UserController.php
@@ -30,9 +30,13 @@ class UserController extends Controller
         //     abort(404);
         // });
 
-        $user = UserModel::findOr(20, ['username' , 'nama'], function (){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username' , 'nama'], function (){
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager9')->firstOrfail();
         return view('user', ['data' => $user]);
     }
 }
